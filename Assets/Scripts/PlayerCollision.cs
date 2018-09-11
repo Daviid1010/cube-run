3d3002424d7877ecd1d8e4f9f3247df3950f61d8
David Sheehy <sheehy_d@ymail.com>
2018-09-11 09:13:28 +0100

    first commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public playerMovement movement;
+
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        Debug.Log(collisionInfo.collider.name);
+
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+
+    //This is a reference to the Rigidbody component in the Player GameObject
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+	// Use this for initialization
+	// Update is called once per frame
+	void FixedUpdate () {                           //fixed update likes for physics unity
+
+        //add a forward force 
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);   //evens out different framerates
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
